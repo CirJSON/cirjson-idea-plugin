e10845b976ce0c02bc32983bf681b003018688ea
Vincent Girard <vinccool96@gmail.com>
2024-04-04 20:49:29 -0400

    Update IDE version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ sourceSets {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.3.2")
+    version.set("2024.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -37,8 +37,8 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("233")
-        untilBuild.set("241.*")
+        sinceBuild.set("241")
+        untilBuild.set("242.*")
     }
 
     signPlugin {

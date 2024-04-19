8519b8a0c1ba812b5a721d22349858b8de18b7f3
Vincent Girard <vinccool96@gmail.com>
2024-04-19 16:57:17 -0400

    Some line moving tests

---
+++ src/test/kotlin/org/cirjson/plugin/idea/CirJsonLineMoverTest.kt
@@ -0,0 +1,69 @@
+package org.cirjson.plugin.idea
+
+import com.intellij.openapi.actionSystem.IdeActions
+
+class CirJsonLineMoverTest : CirJsonTestCase() {
+
+    private fun doTest(down: Boolean) {
+        val testName = getTestName(false)
+        val action = if (down) {
+            IdeActions.ACTION_MOVE_STATEMENT_DOWN_ACTION
+        } else {
+            IdeActions.ACTION_MOVE_STATEMENT_UP_ACTION
+        }
+        val after = if (down) {
+            "afterDown"
+        } else {
+            "afterUp"
+        }
+
+        myFixture.configureByFile("mover/$testName.cirjson")
+        myFixture.performEditorAction(action)
+        myFixture.checkResultByFile("mover/$testName.$after.cirjson")
+    }
+
+    fun testLastArrayElementMovedUp() {
+        doTest(false)
+    }
+
+    fun testLastObjectPropertyMovedUp() {
+        doTest(false)
+    }
+
+    fun testArraySelectionMovedDown() {
+        doTest(true)
+    }
+
+    fun testOutOfScopeHasProp() {
+        doTest(true)
+    }
+
+    fun testOutOfScopeNoFollowing() {
+        doTest(true)
+    }
+
+    fun testStatementSetMovedSameLevelDown() {
+        doTest(true)
+    }
+
+    fun testStatementSetMovedSameLevelUp() {
+        doTest(false)
+    }
+
+    fun testIntoScope() {
+        doTest(false)
+    }
+
+    fun testOutsideArray() {
+        doTest(true)
+    }
+
+    fun testInsideArray() {
+        doTest(false)
+    }
+
+    fun testObjectSelectionMovedDown() {
+        doTest(true)
+    }
+
+}

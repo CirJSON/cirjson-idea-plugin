b8137d3f5c866a7221f15616618b3f323a6dc960
Vincent Girard <vinccool96@gmail.com>
2024-05-04 15:39:57 -0400

    Implemented schema usage trigger collector

--- src/main/kotlin/org/cirjson/plugin/idea/schema/impl/CirJsonSchemaUsageTriggerCollector.kt
+++ src/main/kotlin/org/cirjson/plugin/idea/schema/impl/CirJsonSchemaUsageTriggerCollector.kt
@@ -1,11 +1,25 @@
+@file:Suppress("UnstableApiUsage")
+
 package org.cirjson.plugin.idea.schema.impl
 
-class CirJsonSchemaUsageTriggerCollector {
+import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 
+class CirJsonSchemaUsageTriggerCollector : CounterUsagesCollector() {
+
+    override fun getGroup(): EventLogGroup = GROUP
+
+    @Suppress("CompanionObjectInExtension")
     companion object {
 
+        private val GROUP = EventLogGroup("cirjson.schema", 1)
+
+        private val COMPLETION_BY_SCHEMA_INVOKED = GROUP.registerEvent("completion.by.schema.invoked",
+                EventFields.String("schemaKind", listOf("builtin", "schema", "user", "remote")))
+
         fun trigger(feature: String) {
-            TODO()
+            COMPLETION_BY_SCHEMA_INVOKED.log(feature)
         }
 
     }

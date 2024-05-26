c6aa3b73aae2469262c4c6190adfc93d6fbd253c
Vincent Girard <vinccool96@gmail.com>
2024-05-26 12:58:14 -0400

    Setup CirJsonSchemaMappingsConfigurable

--- src/main/kotlin/org/cirjson/plugin/idea/schema/settings/mappings/CirJsonSchemaMappingsConfigurable.kt
+++ src/main/kotlin/org/cirjson/plugin/idea/schema/settings/mappings/CirJsonSchemaMappingsConfigurable.kt
@@ -3,10 +3,15 @@ package org.cirjson.plugin.idea.schema.settings.mappings
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.options.SearchableConfigurable
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MasterDetailsComponent
+import com.jetbrains.jsonSchema.settings.mappings.TreeUpdater
+import org.cirjson.plugin.idea.CirJsonBundle
 import org.cirjson.plugin.idea.schema.UserDefinedCirJsonSchemaConfiguration
+import java.util.function.Function
 
-open class CirJsonSchemaMappingsConfigurable : MasterDetailsComponent(), SearchableConfigurable, Disposable {
+open class CirJsonSchemaMappingsConfigurable(private val myProject: Project) : MasterDetailsComponent(),
+        SearchableConfigurable, Disposable {
 
     private var myInitializer: Runnable? = null
 
@@ -15,6 +20,30 @@ open class CirJsonSchemaMappingsConfigurable : MasterDetailsComponent(), Searcha
         set(value) {
             myInitializer = value
         }
+
+    private var myError: String? = null
+
+    private val myTreeUpdater = TreeUpdater {
+        TREE_UPDATER.run()
+        updateWarningText(it)
+    }
+
+    private val myNameCreator = Function<String, String> {
+        createUniqueName(it)
+    }
+
+    init {
+        @Suppress("LeakingThis")
+        initTree()
+    }
+
+    private fun updateWarningText(showWarning: Boolean) {
+        TODO()
+    }
+
+    private fun createUniqueName(s: String): String {
+        TODO()
+    }
 
     override fun getDisplayName(): String {
         TODO("Not yet implemented")
@@ -62,6 +91,23 @@ open class CirJsonSchemaMappingsConfigurable : MasterDetailsComponent(), Searcha
 
     fun selectInTree(configuration: UserDefinedCirJsonSchemaConfiguration) {
         TODO()
+    }
+
+    @Suppress("CompanionObjectInExtension")
+    companion object {
+
+        const val SETTINGS_CIRJSON_SCHEMA = "settings.cirjson.schema"
+
+        internal val STUB_SCHEMA_NAME = CirJsonBundle.message("new.schema")
+
+        private val COMPARATOR = Comparator<UserDefinedCirJsonSchemaConfiguration> { o1, o2 ->
+            if (o1.isApplicationDefined != o2.isApplicationDefined) {
+                if (o1.isApplicationDefined) 1 else -1
+            } else {
+                o1.name!!.compareTo(o2.name!!, true)
+            }
+        }
+
     }
 
 }
